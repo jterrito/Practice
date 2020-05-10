4e0fdde58d4bb7c2ae9673ab19f7271cf86dfd0d
jterrito <jennifer.l.territo@gmail.com>
2020-05-10 12:17:15 -0400

    Add git practice file

---
+++ Javascript_Git_Practice.js
@@ -0,0 +1,21 @@
+console.log("Practice makes perfect");
+const num = 10
+if(num>5){
+    console.log("Number is greater than 5");
+}
+let favBook = "Jane Eyre";
+console.log(favBook);//Output: Jane Eyre;
+let a = 10;
+a +=1;
+console.log(a);//Output: 11
+let b = 14;
+b --;
+console.log(b);//Output: 13
+let favAuthor = "Oscar Wilde";
+console.log('My favorite author is {favAuthor}');
+//Output: My favorite author is Oscar Wilde;
+const unknown1 = "literature";
+console.log(typeof unknown1);//Output: string
+const unknown2 = true;
+console.log(typeof unknown3);//Output boolean
+
